ff76fba47e67c896f38ad2a81395ae3749087fb2
genglerjoshua <genglerjoshua@gmail.com>
2024-02-14 17:14:29 -0800

    First Version Complete
    This version is a completed version with working JS calculations.

--- index.js
+++ index.js
@@ -2,14 +2,37 @@ const regInput = document.getElementById('reg-input');
 const otInput = document.getElementById('ot-input');
 const lunchInput = document.getElementById('lunch-input');
 const timeOutput = document.getElementById('time-output');
+const reset = document.getElementById('card-top');
+const calculate = document.getElementById('card-bottom');
 
 // Create a function to convert time input to decimal
 
+//Create a function to reset the inputs when "Reset" is clicked
+reset.addEventListener("click", resetInputs);
+
+function resetInputs() {
+    regInput.value = '';
+    otInput.value = '';
+    lunchInput.value = '';
+    timeOutput.textContent = '';
+}
+
 // Create a function to calculate time out when "caclulate" is clicked
+calculate.addEventListener("click", calc);
 
 function calc() {
-    // 40-(regular + (ot * 1.5)) + lunch clock in
-    time = 40 - (regInput.value + (otInput.value * 1.5)) + lunchInput.value;
+    let [hours, minutes] = lunchInput.value.split(":").map(Number);
+    let clockIn = hours + (minutes / 60);
+    let regTimeNum = parseFloat(regInput.value);
+    let otTimeNum = parseFloat(otInput.value);
 
-    timeOutput.textContent = time;
+    let time = 40 - (regTimeNum + (otTimeNum * 1.5)) + clockIn;
+
+    let hour = Math.floor(time);
+    let decimalMinutes = (time - hour) * 60;
+    let minute = Math.round(decimalMinutes);
+
+    let convertedTime = hour + ":" + (minute < 10 ? "0" : "") + minute + "PM";
+
+    timeOutput.textContent = convertedTime;
 };
